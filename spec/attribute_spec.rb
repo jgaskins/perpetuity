aac9aca21fb514f06d39ea30e66ca978c8907c19
Jamie Gaskins <jgaskins@gmail.com>
2012-05-28 07:31:51 +0800

    Isolate Attribute specs

--- spec/attribute_spec.rb
+++ spec/attribute_spec.rb
@@ -1,13 +1,12 @@
-require 'perpetuity'
-require 'test_classes'
+require 'perpetuity/attribute'
 
 describe Perpetuity::Attribute do
-  subject { Perpetuity::Attribute.new :article, Article }
+  subject { Perpetuity::Attribute.new :article, Object }
   it 'has a name' do
     subject.name.should == :article
   end
 
   it 'has a type' do
-    subject.type.should == Article
+    subject.type.should == Object
   end
 end
