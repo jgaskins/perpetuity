66101940beeb633c354df168ceb94697b2fe45b7
Jamie Gaskins <jgaskins@gmail.com>
2012-05-28 07:15:32 +0800

    Clarify some validations specs

--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,71 +1,74 @@
 require 'perpetuity/validations'
 
-describe Perpetuity::ValidationSet do
-  it 'contains validations' do
-    subject.validations.should_not be_nil
-  end
-
-  it 'can add validations' do
-    o = "Validation"
-    subject << o
-
-    subject.first.should == o
-  end
-
-  it 'validates an object' do
-    o = "Validation"
-    o.stub(pass?: true)
-    subject << o
-
-    subject.valid?("object").should be_true
-  end
-
-  it 'invalidates an object' do
-    o = "Validation"
-    o.stub(pass?: false)
-    subject << o
-
-    subject.valid?("object").should be_false
-  end
-
-  describe 'validation types' do
-    let(:valid_object) { Object.new }
-
-    it 'validates presence of an attribute' do
-      valid_object.stub(email: 'me@example.com')
-      subject.present :email
-
-      subject.count.should == 1
-      subject.valid?(valid_object).should be_true
+module Perpetuity
+  describe ValidationSet do
+    let(:validation_set) { ValidationSet.new }
+    it 'is empty when created' do
+      validation_set.validations.should be_empty
     end
 
-    it 'validates length of an attribute' do
-      valid_object.stub(email: 'me@example.com')
-      subject.length :email, at_most: 14
-      subject.length :email, at_least: 14
+    it 'can add validations' do
+      v = double('validation')
+      validation_set << v
 
-      subject.count.should == 2
-      subject.valid?(valid_object).should be_true
+      validation_set.first.should == v
     end
 
-    it 'invalidates when attribute is too short' do
-      valid_object.stub(email: 'foo')
-      subject.length :email, at_least: 4
+    it 'validates an object' do
+      v = double('validation')
+      v.stub(pass?: true)
+      validation_set << v
 
-      subject.count.should == 1
-      subject.valid?(valid_object).should be_false
+      validation_set.valid?(double('object')).should be_true
     end
 
-    it 'invalidates when attribute is too long' do
-      valid_object.stub(email: 'me@example.com')
-      subject.length :email, at_most: 4
+    it 'invalidates an object' do
+      v = double('validation')
+      v.stub(pass?: false)
+      validation_set << v
 
-      subject.count.should == 1
-      subject.valid?(valid_object).should be_false
+      validation_set.valid?(double('object')).should be_false
+    end
+
+    describe 'validation types' do
+      let(:valid_object) { double('valid object') }
+
+      it 'validates presence of an attribute' do
+        valid_object.stub(email: 'me@example.com')
+        validation_set.present :email
+
+        validation_set.count.should == 1
+        validation_set.valid?(valid_object).should be_true
+      end
+
+      it 'validates length of an attribute' do
+        valid_object.stub(email: 'me@example.com')
+        validation_set.length :email, at_most: 14
+        validation_set.length :email, at_least: 14
+
+        validation_set.count.should == 2
+        validation_set.valid?(valid_object).should be_true
+      end
+
+      it 'invalidates when attribute is too short' do
+        valid_object.stub(email: 'foo')
+        validation_set.length :email, at_least: 4
+
+        validation_set.count.should == 1
+        validation_set.valid?(valid_object).should be_false
+      end
+
+      it 'invalidates when attribute is too long' do
+        valid_object.stub(email: 'me@example.com')
+        subject.length :email, at_most: 4
+
+        subject.count.should == 1
+        subject.valid?(valid_object).should be_false
+      end
     end
   end
-end
 
-describe Perpetuity::Validation do
+  describe Perpetuity::Validation do
 
+  end
 end
