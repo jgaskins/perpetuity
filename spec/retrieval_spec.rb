2a435aaeef3834ecce3254002861727916e258ac
Jamie Gaskins <jgaskins@gmail.com>
2012-05-28 06:59:12 +0800

    Simplify retrieval sorting spec

--- spec/retrieval_spec.rb
+++ spec/retrieval_spec.rb
@@ -2,7 +2,8 @@ require "perpetuity"
 require "test_classes"
 
 describe Perpetuity::Retrieval do
-  subject { Perpetuity::Retrieval.new Article, {} }
+  let(:retrieval) { Perpetuity::Retrieval.new Article, {} }
+  subject { retrieval }
 
   before(:all) do
     Perpetuity.configure do
@@ -16,19 +17,13 @@ describe Perpetuity::Retrieval do
   end
 
   it "sorts the results" do
-    ArticleMapper.insert Article.new('B')
-    ArticleMapper.insert Article.new('A')
-    ArticleMapper.insert Article.new('C')
-    
-    subject.sort(:title).map(&:title).should == %w(A B C)
+    sorted = retrieval.sort(:name)
+    sorted.sort_attribute.should == :name
   end
 
   it "reverses the sort order of the results" do
-    ArticleMapper.insert Article.new('B')
-    ArticleMapper.insert Article.new('A')
-    ArticleMapper.insert Article.new('C')
-    
-    subject.sort(:title).reverse.map(&:title).should == %w(C B A)
+    sorted = retrieval.sort(:name).reverse
+    sorted.sort_direction.should == :descending
   end
   
   it "limits the result set" do
