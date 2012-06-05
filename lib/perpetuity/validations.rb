fe981e696d51b6cc8b5840967f7a47eac5bfeaaa
Jamie Gaskins <jgaskins@gmail.com>
2012-06-05 20:54:36 +0800

    Use dynamic method dispatch for length validation

--- lib/perpetuity/validations.rb
+++ lib/perpetuity/validations.rb
@@ -36,21 +36,31 @@ module Perpetuity
     class Length
       def initialize attribute, options
         @attribute = attribute
-        @options = options
-
-        if range = options.delete(:between)
-          @options[:at_least] ||= range.min
-          @options[:at_most] ||= range.max
+        options.each do |option, value|
+          send option, value
         end
       end
 
       def pass? object
         length = object.send(@attribute).length
 
-        return false unless @options[:at_least].nil? or @options[:at_least] <= length
-        return false unless @options[:at_most].nil? or @options[:at_most] >= length
+        return false unless @at_least.nil? or @at_least <= length
+        return false unless @at_most.nil? or @at_most >= length
 
         true
+      end
+
+      def at_least value
+        @at_least = value
+      end
+
+      def at_most value
+        @at_most = value
+      end
+
+      def between range
+        at_least range.min
+        at_most range.max
       end
     end
   end
