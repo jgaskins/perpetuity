79db69f0a8be8b8d824e2de15af6b95ca61c4560
Jamie Gaskins <jgaskins@gmail.com>
2012-10-19 02:18:38 +0800

    Convert FooMapper to Mapper[Foo] in spec

--- spec/perpetuity_spec.rb
+++ spec/perpetuity_spec.rb
@@ -11,19 +11,19 @@ describe Perpetuity do
   describe 'persistence' do
     it "persists an object" do
       article = Article.new 'I have a title'
-      expect { ArticleMapper.insert article }.
-        to change { ArticleMapper.count }.by 1
-      ArticleMapper.find(article.id).title.should eq 'I have a title'
+      expect { Perpetuity::Mapper[Article].insert article }.
+        to change { Perpetuity::Mapper[Article].count }.by 1
+      Perpetuity::Mapper[Article].find(article.id).title.should eq 'I have a title'
     end
 
     it 'returns the id of the persisted object' do
       article = Article.new
-      ArticleMapper.insert(article).should eq article.id
+      Perpetuity::Mapper[Article].insert(article).should eq article.id
     end
 
     it "gives an id to objects" do
       article = Article.new
-      ArticleMapper.give_id_to article, 1
+      Perpetuity::Mapper[Article].give_id_to article, 1
 
       article.id.should eq 1
     end
@@ -32,20 +32,20 @@ describe Perpetuity do
       let(:article) { Article.new }
 
       it 'assigns an id to the inserted object' do
-        ArticleMapper.insert article
+        Perpetuity::Mapper[Article].insert article
         article.should respond_to :id
       end
 
       it "assigns an id using Mapper.first" do
-        ArticleMapper.first.should respond_to :id
+        Perpetuity::Mapper[Article].first.should respond_to :id
       end
 
       it 'assigns an id using Mapper.retrieve.first' do
-        ArticleMapper.retrieve.first.should respond_to :id
+        Perpetuity::Mapper[Article].retrieve.first.should respond_to :id
       end
 
       it 'assigns an id using Mapper.all.first' do
-        ArticleMapper.all.first.should respond_to :id
+        Perpetuity::Mapper[Article].all.first.should respond_to :id
       end
     end
 
@@ -54,15 +54,15 @@ describe Perpetuity do
 
       it 'persists arrays' do
         article.comments << 1 << 2 << 3
-        ArticleMapper.insert article
-        ArticleMapper.find(article.id).comments.should eq [1, 2, 3]
+        Perpetuity::Mapper[Article].insert article
+        Perpetuity::Mapper[Article].find(article.id).comments.should eq [1, 2, 3]
       end
 
       it 'persists arrays with unserializable objects in them' do
         comment = Comment.new('my comment')
         article.comments << comment
-        ArticleMapper.insert article
-        ArticleMapper.find(article.id).comments.first.tap do |persisted_comment|
+        Perpetuity::Mapper[Article].insert article
+        Perpetuity::Mapper[Article].find(article.id).comments.first.tap do |persisted_comment|
           persisted_comment.should be_a Comment
           persisted_comment.body.should eq comment.body
         end
@@ -72,52 +72,52 @@ describe Perpetuity do
     it "allows mappers to set the id field" do
       book = Book.new('My Title')
 
-      BookMapper.insert book
+      Perpetuity::Mapper[Book].insert book
       book.id.should eq 'my-title'
     end
   end
 
   describe "deletion" do
     it 'deletes an object' do
-      2.times { ArticleMapper.insert Article.new }
-      expect { ArticleMapper.delete ArticleMapper.first }.to change { ArticleMapper.count }.by(-1)
+      2.times { Perpetuity::Mapper[Article].insert Article.new }
+      expect { Perpetuity::Mapper[Article].delete Perpetuity::Mapper[Article].first }.to change { Perpetuity::Mapper[Article].count }.by(-1)
     end
 
     it 'deletes an object with a given id' do
-      article_id = ArticleMapper.insert Article.new
+      article_id = Perpetuity::Mapper[Article].insert Article.new
       expect {
-        ArticleMapper.delete article_id
-      }.to change { ArticleMapper.count }.by(-1)
+        Perpetuity::Mapper[Article].delete article_id
+      }.to change { Perpetuity::Mapper[Article].count }.by(-1)
     end
     
     describe "#delete_all" do
       it "should delete all objects of a certain class" do
-        ArticleMapper.insert Article.new
-        ArticleMapper.delete_all
-        ArticleMapper.count.should eq 0
+        Perpetuity::Mapper[Article].insert Article.new
+        Perpetuity::Mapper[Article].delete_all
+        Perpetuity::Mapper[Article].count.should eq 0
       end
     end
   end
 
   describe "retrieval" do
     it "gets all the objects of a class" do
-      expect { ArticleMapper.insert Article.new }.
-        to change { ArticleMapper.all.count }.by 1
+      expect { Perpetuity::Mapper[Article].insert Article.new }.
+        to change { Perpetuity::Mapper[Article].all.count }.by 1
     end
     
     it "has an ID when retrieved" do
-      ArticleMapper.insert Article.new
-      ArticleMapper.first.should respond_to :id
+      Perpetuity::Mapper[Article].insert Article.new
+      Perpetuity::Mapper[Article].first.should respond_to :id
     end
     
     it "returns a Perpetuity::Retrieval object" do
-      ArticleMapper.retrieve(id: 1).should be_an_instance_of Perpetuity::Retrieval
+      Perpetuity::Mapper[Article].retrieve(id: 1).should be_an_instance_of Perpetuity::Retrieval
     end
 
     it "gets an item with a specific ID" do
       article = Article.new
-      ArticleMapper.insert article
-      retrieved = ArticleMapper.find(article.id)
+      Perpetuity::Mapper[Article].insert article
+      retrieved = Perpetuity::Mapper[Article].find(article.id)
 
       retrieved.id.should eq article.id
       retrieved.title.should eq article.title
@@ -126,8 +126,8 @@ describe Perpetuity do
 
     it "gets an item by its attributes" do
       article = Article.new
-      ArticleMapper.insert article
-      retrieved = ArticleMapper.retrieve(title: article.title)
+      Perpetuity::Mapper[Article].insert article
+      retrieved = Perpetuity::Mapper[Article].retrieve(title: article.title)
 
       retrieved.to_a.should_not be_empty
       retrieved.first.title.should eq article.title
@@ -137,60 +137,60 @@ describe Perpetuity do
       let(:draft) { Article.new 'Draft', 'draft content', nil, Time.now + 30 }
       let(:published) { Article.new 'Published', 'content', nil, Time.now - 30, 3 }
       before do
-        ArticleMapper.insert draft
-        ArticleMapper.insert published
+        Perpetuity::Mapper[Article].insert draft
+        Perpetuity::Mapper[Article].insert published
       end
 
       it 'selects objects using equality' do
-        selected = ArticleMapper.select { title == 'Published' }
+        selected = Perpetuity::Mapper[Article].select { title == 'Published' }
         selected.map(&:id).should include published.id
         selected.map(&:id).should_not include draft.id
       end
 
       it 'selects objects using greater-than' do
-        selected = ArticleMapper.select { published_at < Time.now }
+        selected = Perpetuity::Mapper[Article].select { published_at < Time.now }
         ids = selected.map(&:id)
         ids.should include published.id
         ids.should_not include draft.id
       end
 
       it 'selects objects using greater-than-or-equal' do
-        selected = ArticleMapper.select { views >= 3 }
+        selected = Perpetuity::Mapper[Article].select { views >= 3 }
         ids = selected.map(&:id)
         ids.should include published.id
         ids.should_not include draft.id
       end
 
       it 'selects objects using less-than' do
-        selected = ArticleMapper.select { views < 3 }
+        selected = Perpetuity::Mapper[Article].select { views < 3 }
         ids = selected.map(&:id)
         ids.should include draft.id
         ids.should_not include published.id
       end
 
       it 'selects objects using less-than-or-equal' do
-        selected = ArticleMapper.select { views <= 0 }
+        selected = Perpetuity::Mapper[Article].select { views <= 0 }
         ids = selected.map(&:id)
         ids.should include draft.id
         ids.should_not include published.id
       end
 
       it 'selects objects using inequality' do
-        selected = ArticleMapper.select { title.not_equal? 'Draft' }
+        selected = Perpetuity::Mapper[Article].select { title.not_equal? 'Draft' }
         ids = selected.map(&:id)
         ids.should_not include draft.id
         ids.should include published.id
       end
 
       it 'selects objects using regular expressions' do
-        selected = ArticleMapper.select { title =~ /Pub/ }
+        selected = Perpetuity::Mapper[Article].select { title =~ /Pub/ }
         ids = selected.map(&:id)
         ids.should include published.id
         ids.should_not include draft.id
       end
 
       it 'selects objects using inclusion' do
-        selected = ArticleMapper.select { title.in %w( Published ) }
+        selected = Perpetuity::Mapper[Article].select { title.in %w( Published ) }
         ids = selected.map(&:id)
         ids.should include published.id
         ids.should_not include draft.id
@@ -200,22 +200,22 @@ describe Perpetuity do
 
   describe 'pagination' do
     it 'specifies the page we want' do
-      ArticleMapper.retrieve.should respond_to :page
+      Perpetuity::Mapper[Article].retrieve.should respond_to :page
     end
 
     it 'specify the quantity per page' do
-      ArticleMapper.retrieve.should respond_to :per_page
+      Perpetuity::Mapper[Article].retrieve.should respond_to :per_page
     end
 
     it 'returns an empty set when there is no data for that page' do
-      data = ArticleMapper.retrieve.page(2)
+      data = Perpetuity::Mapper[Article].retrieve.page(2)
       data.should be_empty
     end
 
     it 'specifies per-page quantity' do
-      ArticleMapper.delete_all
-      5.times { |i| ArticleMapper.insert Article.new i }
-      data = ArticleMapper.retrieve.page(3).per_page(2)
+      Perpetuity::Mapper[Article].delete_all
+      5.times { |i| Perpetuity::Mapper[Article].insert Article.new i }
+      data = Perpetuity::Mapper[Article].retrieve.page(3).per_page(2)
       data.should have(1).item
     end
   end
@@ -238,18 +238,18 @@ describe Perpetuity do
       topic.creator = user
       topic.title = 'Title'
 
-      UserMapper.insert user
-      TopicMapper.insert topic
+      Perpetuity::Mapper[User].insert user
+      Perpetuity::Mapper[Topic].insert topic
     end
 
     it 'can reference other objects' do
-      TopicMapper.find(topic.id).creator.should eq user.id
+      Perpetuity::Mapper[Topic].find(topic.id).creator.should eq user.id
     end
 
     it 'can retrieve associated objects' do
-      retrieved_topic = TopicMapper.first
+      retrieved_topic = Perpetuity::Mapper[Topic].first
 
-      TopicMapper.load_association! retrieved_topic, :creator
+      Perpetuity::Mapper[Topic].load_association! retrieved_topic, :creator
       retrieved_topic.creator.name.should eq 'Flump'
     end
   end
@@ -257,12 +257,12 @@ describe Perpetuity do
   describe 'updating' do
     let(:article) { Article.new }
     before do
-      ArticleMapper.insert article
+      Perpetuity::Mapper[Article].insert article
     end
 
     it 'updates an object in the database' do
-      ArticleMapper.update article, title: 'I has a new title!'
-      ArticleMapper.find(article.id).title.should eq 'I has a new title!'
+      Perpetuity::Mapper[Article].update article, title: 'I has a new title!'
+      Perpetuity::Mapper[Article].find(article.id).title.should eq 'I has a new title!'
     end
   end
 
@@ -283,21 +283,21 @@ describe Perpetuity do
 
     it 'raises an exception when inserting an invalid object' do
       car = Car.new
-      expect { CarMapper.insert car }.to raise_error
+      expect { Perpetuity::Mapper[Car].insert car }.to raise_error
     end
 
     it 'does not raise an exception when validations are met' do
       car = Car.new
       car.make = "Volkswagen"
-      expect { CarMapper.insert car }.not_to raise_error
+      expect { Perpetuity::Mapper[Car].insert car }.not_to raise_error
     end
   end
 
   # The Message class stores its data members differently internally than it receives them
   it 'uses accessor methods to read/write data' do
     message = Message.new 'My Message!'
-    MessageMapper.insert message
-    saved_message = MessageMapper.find(message.id)
+    Perpetuity::Mapper[Message].insert message
+    saved_message = Perpetuity::Mapper[Message].find(message.id)
     saved_message.instance_variable_get(:@text).should eq 'My Message!'.reverse
     saved_message.text.should eq 'My Message!'
   end
