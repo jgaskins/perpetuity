cfe6d5967c2d93acff12276fb25d3ccff6497592
Jamie Gaskins <jgaskins@gmail.com>
2013-01-27 02:26:45 +0800

    Make default test class data index-friendly

--- spec/support/test_classes/book.rb
+++ spec/support/test_classes/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :title, :authors
-  def initialize title="Foo Bar", authors=[]
+  def initialize title="Foo Bar #{Time.now.to_f.to_s}", authors=[]
     @title = title
     @authors = authors
   end
