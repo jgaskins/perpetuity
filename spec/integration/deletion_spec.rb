1fe3cee69579efdff3ca9ca9863c2cbc38702349
Jamie Gaskins <jgaskins@gmail.com>
2013-02-10 05:57:12 +0800

    Extract deletion specs to their own file

---
+++ spec/integration/deletion_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'support/test_classes'
+
+describe "deletion" do
+  it 'deletes an object' do
+    2.times { Perpetuity[Article].insert Article.new }
+    expect { Perpetuity[Article].delete Perpetuity[Article].first }.to change { Perpetuity[Article].count }.by(-1)
+  end
+
+  it 'deletes an object with a given id' do
+    article_id = Perpetuity[Article].insert Article.new
+    expect {
+      Perpetuity[Article].delete article_id
+    }.to change { Perpetuity[Article].count }.by(-1)
+  end
+
+  describe "#delete_all" do
+    it "should delete all objects of a certain class" do
+      Perpetuity[Article].insert Article.new
+      Perpetuity[Article].delete_all
+      Perpetuity[Article].count.should eq 0
+    end
+  end
+end
+
+
--- spec/perpetuity_spec.rb
+++ spec/perpetuity_spec.rb
@@ -22,28 +22,6 @@ describe Perpetuity do
     end
   end
 
-  describe "deletion" do
-    it 'deletes an object' do
-      2.times { Perpetuity[Article].insert Article.new }
-      expect { Perpetuity[Article].delete Perpetuity[Article].first }.to change { Perpetuity[Article].count }.by(-1)
-    end
-
-    it 'deletes an object with a given id' do
-      article_id = Perpetuity[Article].insert Article.new
-      expect {
-        Perpetuity[Article].delete article_id
-      }.to change { Perpetuity[Article].count }.by(-1)
-    end
-    
-    describe "#delete_all" do
-      it "should delete all objects of a certain class" do
-        Perpetuity[Article].insert Article.new
-        Perpetuity[Article].delete_all
-        Perpetuity[Article].count.should eq 0
-      end
-    end
-  end
-
   describe "retrieval" do
     it "gets all the objects of a class" do
       expect { Perpetuity[Article].insert Article.new }.
