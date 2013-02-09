c9cb279279ca362f21a8548ff050bb6082c64c17
Jamie Gaskins <jgaskins@gmail.com>
2013-02-10 06:09:27 +0800

    Extract serialization integration spec

--- spec/perpetuity_spec.rb
+++ spec/perpetuity_spec.rb
@@ -22,64 +22,6 @@ describe Perpetuity do
     end
   end
 
-  describe 'serialization' do
-    let(:author) { User.new 'username' }
-    let(:comment) { Comment.new }
-    let(:article) { Article.new }
-    let(:mapper) { Perpetuity[Article] }
-    let(:serialized_value) do
-      {
-        'title' => article.title,
-        'body' => article.body,
-        'author' => {
-          '__metadata__' => {
-            'class' => author.class.to_s,
-            'id' => author.id
-          }
-        },
-        'comments' => [
-          {
-            '__metadata__' => {
-              'class' => comment.class.to_s
-            },
-            'body' => comment.body,
-            'author' => {
-              '__metadata__' => {
-                'class' => author.class.to_s,
-                'id' => author.id
-              }
-            }
-          },
-        ],
-        'published_at' => article.published_at,
-        'views' => article.views
-      } 
-    end
-
-    before do
-      article.author = author
-      article.comments = [comment]
-      comment.author = author
-
-      Perpetuity[User].insert author
-      Perpetuity[Article].insert article
-    end
-
-    it 'serializes objects into hashes' do
-      mapper.serialize(article).should be == serialized_value
-    end
-
-    it 'deserializes hashes into proper objects' do
-      unserialized = mapper.find article.id
-      unserialized.should be_a Article
-      unserialized.title.should be == article.title
-      unserialized.body.should be == article.body
-      unserialized.comments.first.tap do |unserialized_comment|
-        unserialized_comment.body.should be == comment.body
-      end
-    end
-  end
-
   describe 'methods on mappers' do
     it 'allows methods to act as scopes' do
       published = Article.new('Published', 'I love cats', nil, Time.now - 30)
