a36b718e49e996d981eaf7ab8f70a9f0dfb59f89
Jamie Gaskins <jgaskins@gmail.com>
2013-02-10 06:02:23 +0800

    Extract pagination specs to their own file

--- spec/perpetuity_spec.rb
+++ spec/perpetuity_spec.rb
@@ -22,30 +22,6 @@ describe Perpetuity do
     end
   end
 
-  describe 'pagination' do
-    it 'specifies the page we want' do
-      Perpetuity[Article].all.should respond_to :page
-    end
-
-    it 'specify the quantity per page' do
-      Perpetuity[Article].all.should respond_to :per_page
-    end
-
-    it 'returns an empty set when there is no data for that page' do
-      mapper = Perpetuity[Article]
-      mapper.delete_all
-      data = mapper.all.page(2)
-      data.should be_empty
-    end
-
-    it 'specifies per-page quantity' do
-      Perpetuity[Article].delete_all
-      5.times { |i| Perpetuity[Article].insert Article.new i }
-      data = Perpetuity[Article].all.page(3).per_page(2)
-      data.should have(1).item
-    end
-  end
-
   describe 'associations with other objects' do
     let(:user) { User.new }
     let(:topic) { Topic.new }
