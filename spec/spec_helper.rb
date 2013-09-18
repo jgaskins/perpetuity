7c7a490c33d2f95e8d70b2204233762495d8c5c1
Jamie Gaskins <jgaskins@gmail.com>
2013-09-18 17:59:05 -0400

    Allow specs to be run with Postgres adapter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'perpetuity'
 
-Perpetuity.data_source :mongodb, 'perpetuity_gem_test'
+if ENV['PERPETUITY_ADAPTER'] == 'postgres'
+  Perpetuity.data_source :postgres, 'perpetuity_gem_test', user: ENV['USER'], password: nil
+else
+  Perpetuity.data_source :mongodb, 'perpetuity_gem_test'
+end
